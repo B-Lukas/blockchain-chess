21f88d80391790a0bf23c218ad7ef3d7609ff1c6
Lukas B <P0sa@users.noreply.github.com>
2018-05-01 18:42:21 +0200

    Update Prototyp.sol

--- Prototyp.sol
+++ Prototyp.sol
@@ -1,30 +1,29 @@
-pragma solidity ^0.4.0;
-
+pragma solidity ^0.4.23;
 
 
 contract Prototyp {
+    
+    uint counter = 0;                                                                    //muss nach jedem Spiel resettet werden
 
-    /*      struct Move {     //   function addtoarray(Move a) noch nicht moeglich?
-              uint fromX;
-              uint fromY;
-              uint toX;
-              uint toY;
-      }
-      Move[]  moves = new Move[](8);
+    uint[100][3] moves; 
+    
+    function addtoArray(uint fromX, uint fromY, uint toX, uint toY) public payable{      //Memory ist teuer, alle Züge auf einmal Speichern, ansonsten memory und dann zu Storage! + Confirmation Time?
 
-      */
-
-    uint[] moves = new uint[](8);    // TODO: Arrays variabler laenge? nicht moeglich? matrizen nicht moeglich?
-
-
-    function addtoarray(uint fromX, uint fromY, uint toX, uint toY) public payable {
-
-        moves[1] = fromX;
-        moves[2] = fromY;
-        moves[3] = toX;
-        moves[4] = toY;
+        moves[counter][0]  = fromX;
+        moves[counter][1]  = fromY;
+        moves[counter][2]  = toX;
+        moves[counter][3]  = toY;
+        
+        counter = counter + 1;
 
     }
-    //TODO: getMoves (Replay, Saves)
-}
-
+    
+    function getMove(uint x, uint y) public constant returns (uint)                      //Nur zum Testen, Werte in dem Array auslesen
+    {
+    	return moves[x][y];                                                              //x = Zug (startet bei 0)  y geht von 0-3 (fromX,FromY,toX,toY)
+    }
+    
+    function restartGame() public{                                                       //Counter neustarten um das Array wieder von vorne zu befüllen. -> Ist es nötig das Array zu leeren(Kosten)? + Counter gibt die Anzahl der Züge für Replay etc an.
+         counter = 0;  
+         
+    }
